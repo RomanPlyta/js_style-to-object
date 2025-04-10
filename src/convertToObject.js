3d912388abab5e6998db60307430dd3fc0be44a7
Admin <romanpluta42@gmail.com>
2025-04-10 21:28:21 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const sortSourceString = sourceString;
+  const parametr = sortSourceString.split(';');
+  const result = {};
+
+  for (let i = 0; i < parametr.length; i++) {
+    const kayAndValue = parametr[i].split(':');
+    const kay = kayAndValue[0];
+    const value = kayAndValue[1];
+
+    if (value !== undefined) {
+      result[kay.replace(/^\s+|\s+$/g, '')] = value.replace(/^\s+|\s+$/g, '');
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
